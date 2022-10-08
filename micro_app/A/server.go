61a0f8ad64bb7439c6f87b7547e05b52ccd4497e
Ehsan Saei <71217171+esigo@users.noreply.github.com>
2022-10-08 13:06:25 +0200

    service to service (#7)

--- micro_app/A/server.go
+++ micro_app/A/server.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"io"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
@@ -12,20 +15,43 @@ func HandleRoot(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello From myapp Service!\n"))
 }
 
-func HandleB(w http.ResponseWriter, r *http.Request) {
-	url := "http://microapp-service-b.myapp.svc.cluster.local:80/test"
-	http.NewRequest("GET", url, nil)
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		log.Println(err)
+type Payload struct {
+	S string `json:"S"`
+}
+
+func HandleHello(w http.ResponseWriter, r *http.Request) {
+	name := ""
+	paths := strings.Split(r.URL.Path, "/")
+	if len(paths) > 2 {
+		name = paths[2]
 	}
+	data := Payload{
+		S: name,
+	}
+	payloadBytes, err := json.Marshal(data)
+	if err != nil {
+		w.Write([]byte("can't read the name!\n"))
+		return
+	}
+	body := bytes.NewReader(payloadBytes)
+	url := "http://microapp-service-b.myapp.svc.cluster.local:80/hello"
+
+	req, err := http.NewRequest("GET", url, body)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+
 	req.Header = r.Header
+	req.Header.Set("Content-Type", "application/json")
+
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Println(err)
 	}
 	defer resp.Body.Close()
+
 	if resp.StatusCode == http.StatusOK {
 		bodyBytes, err := io.ReadAll(resp.Body)
 		if err != nil {
@@ -42,6 +68,7 @@ func HandleB(w http.ResponseWriter, r *http.Request) {
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", HandleRoot)
+	r.HandleFunc("/hello/{name:[a-z]+}", HandleHello)
 
 	log.Fatal(http.ListenAndServe(":80", r))
 }

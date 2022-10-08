7d9bb1268d8e97b4967799018d6916e78b034eb0
Ehsan Saei <71217171+esigo@users.noreply.github.com>
2022-10-08 11:01:06 +0200

    add service b

---
+++ micro_app/B/go.mod
@@ -0,0 +1,10 @@
+module service-b
+
+go 1.19
+
+require github.com/go-kit/kit v0.12.0
+
+require (
+	github.com/go-kit/log v0.2.0 // indirect
+	github.com/go-logfmt/logfmt v0.5.1 // indirect
+)
---
+++ micro_app/B/server.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/go-kit/kit/endpoint"
+	httptransport "github.com/go-kit/kit/transport/http"
+)
+
+type HelloService interface {
+	Hello(string) (string, error)
+}
+
+type helloService struct{}
+
+func (helloService) Hello(s string) (string, error) {
+	if s == "" {
+		return "", ErrEmpty
+	}
+	var sb strings.Builder
+
+	sb.WriteString("hello ")
+	sb.WriteString(s)
+	sb.WriteString("!")
+	return sb.String(), nil
+}
+
+var ErrEmpty = errors.New("Empty string")
+
+type helloRequest struct {
+	S string `json:"s"`
+}
+
+type helloResponse struct {
+	V   string `json:"v"`
+	Err string `json:"err,omitempty"`
+}
+
+func makeHelloEndpoint(svc HelloService) endpoint.Endpoint {
+	return func(_ context.Context, request interface{}) (interface{}, error) {
+		req := request.(helloRequest)
+		v, err := svc.Hello(req.S)
+		if err != nil {
+			return helloResponse{v, err.Error()}, nil
+		}
+		return helloResponse{v, ""}, nil
+	}
+}
+
+func decodeHelloRequest(_ context.Context, r *http.Request) (interface{}, error) {
+	var request helloRequest
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		return nil, err
+	}
+	return request, nil
+}
+
+func encodeResponse(_ context.Context, w http.ResponseWriter, response interface{}) error {
+	return json.NewEncoder(w).Encode(response)
+}
+
+func main() {
+	svc := helloService{}
+
+	helloHandler := httptransport.NewServer(
+		makeHelloEndpoint(svc),
+		decodeHelloRequest,
+		encodeResponse,
+	)
+
+	http.Handle("/hello", helloHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}

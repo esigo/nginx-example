a18f4a61de7be9d8463eb50c279ac718e2b7af86
Ehsan Saei <71217171+esigo@users.noreply.github.com>
2022-10-08 17:27:12 +0200

    instrument

--- micro_app/B/server.go
+++ micro_app/B/server.go
@@ -4,12 +4,28 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"strings"
+	"time"
 
 	"github.com/go-kit/kit/endpoint"
 	httptransport "github.com/go-kit/kit/transport/http"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.12.0"
+	"go.opentelemetry.io/otel/trace"
 )
 
 type HelloService interface {
@@ -42,8 +58,13 @@ type helloResponse struct {
 }
 
 func makeHelloEndpoint(svc HelloService) endpoint.Endpoint {
-	return func(_ context.Context, request interface{}) (interface{}, error) {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
 		req := request.(helloRequest)
+		span := trace.SpanFromContext(ctx)
+		span.SetAttributes(
+			attribute.String("attr", "serve-b"),
+		)
+		defer span.End()
 		v, err := svc.Hello(req.S)
 		if err != nil {
 			return helloResponse{v, err.Error()}, nil
@@ -52,7 +73,14 @@ func makeHelloEndpoint(svc HelloService) endpoint.Endpoint {
 	}
 }
 
-func decodeHelloRequest(_ context.Context, r *http.Request) (interface{}, error) {
+func decodeHelloRequest(ctx context.Context, r *http.Request) (interface{}, error) {
+	propagator := otel.GetTextMapPropagator()
+	ctx = propagator.Extract(r.Context(), propagation.HeaderCarrier(r.Header))
+	_, span := otel.Tracer("service-b/hello").Start(ctx, "service-b hello")
+	span.SetAttributes(
+		attribute.String("attr", "serve-b"),
+	)
+	defer span.End()
 	var request helloRequest
 	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
 		return nil, err
@@ -64,7 +92,55 @@ func encodeResponse(_ context.Context, w http.ResponseWriter, response interface
 	return json.NewEncoder(w).Encode(response)
 }
 
+func initProvider() (func(context.Context) error, error) {
+	ctx := context.Background()
+
+	res, err := resource.New(ctx,
+		resource.WithAttributes(
+			semconv.ServiceNameKey.String("microapp-service-b"),
+		),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create resource: %w", err)
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, time.Second)
+	defer cancel()
+	conn, err := grpc.DialContext(ctx, "otel-coll-collector.otel.svc:4317", grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
+	if err != nil {
+		return nil, fmt.Errorf("failed to create gRPC connection to collector: %w", err)
+	}
+
+	traceExporter, err := otlptracegrpc.New(ctx, otlptracegrpc.WithGRPCConn(conn))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create trace exporter: %w", err)
+	}
+
+	bsp := sdktrace.NewBatchSpanProcessor(traceExporter)
+	tracerProvider := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithResource(res),
+		sdktrace.WithSpanProcessor(bsp),
+	)
+	otel.SetTracerProvider(tracerProvider)
+
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
+
+	return tracerProvider.Shutdown, nil
+}
+
 func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+	shutdown, err := initProvider()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := shutdown(ctx); err != nil {
+			log.Fatal("failed to shutdown TracerProvider: %w", err)
+		}
+	}()
 	svc := helloService{}
 
 	helloHandler := httptransport.NewServer(
